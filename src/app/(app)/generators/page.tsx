c2adf5990013fd390d9fc92c79b1a371d06ce214
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-02-10 15:35:37 +0100

    feat: topic page for each kind of tool

--- src/app/(app)/generators/page.tsx
+++ src/app/(app)/generators/page.tsx
@@ -1,7 +1,19 @@
+import { tools } from '@/app/tools'
+import { ToolCard } from '@/components/tool-card'
+
 export default function GeneratorsPage() {
+  const generators = tools.filter((tools) => tools.category === 'generators')[0]
+    .items
+
   return (
-    <div>
-      <h1>Generators page</h1>
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold font-mono">Geradores</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+        {generators.map((tool) => (
+          <ToolCard key={tool.title} {...tool} />
+        ))}
+      </div>
     </div>
   )
 }
