fc55031a2ae1ee2638accd853f0c1e00aa80e8e0
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-02-10 21:20:14 +0100

    feat: keywords for indexing

--- src/app/(app)/generators/cpf/layout.tsx
+++ src/app/(app)/generators/cpf/layout.tsx
@@ -4,6 +4,14 @@ import type { ReactNode } from 'react'
 export const metadata: Metadata = {
   title: 'Gerador de CPF',
   description: 'Gere um número de CPF válido instantaneamente.',
+  keywords: [
+    'gerador de cpf',
+    'gerador de cpf online',
+    'gerador de cpf válido',
+    'gerador de cpf com números',
+    'gerador de cpf com pontuação',
+    'gerador de cpf com máscara',
+  ],
 }
 
 export default function CPFGeneratorLayout({
--- src/app/(app)/generators/password/layout.tsx
+++ src/app/(app)/generators/password/layout.tsx
@@ -5,6 +5,19 @@ export const metadata: Metadata = {
   title: 'Gerador de Senha',
   description:
     'Gere senhas seguras e aleatórias com diferentes combinações de caracteres, incluindo letras maiúsculas, números e símbolos especiais.',
+  keywords: [
+    'gerador de senha',
+    'gerador de senha online',
+    'gerador de senha segura',
+    'gerador de senha aleatória',
+    'gerador de senha forte',
+    'gerador de senha com letras e números',
+    'gerador de senha com letras maiúsculas',
+    'gerador de senha com letras minúsculas',
+    'gerador de senha com números',
+    'gerador de senha com símbolos',
+    'gerador de senha com caracteres especiais',
+  ],
 }
 
 export default function PasswordGeneratorLayout({
