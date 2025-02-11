3820a3a29026097c4ec856061467957a2269f704
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-02-11 13:59:20 +0100

    feat: how to use the API

--- src/app/api/validators/cpf/route.ts
+++ src/app/api/validators/cpf/route.ts
@@ -6,9 +6,5 @@ export async function POST(request: Request) {
 
   const status = validateCPF(cpf)
 
-  if (!status) {
-    return NextResponse.json({ status: false }, { status: 400 })
-  }
-
   return NextResponse.json({ status })
 }
