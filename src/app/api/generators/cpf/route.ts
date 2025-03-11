770346001c3513c6987e40cdf590db6e594131b0
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-03-11 19:47:48 +0100

    chore: move generateCpf function to lib

--- src/app/api/generators/cpf/route.ts
+++ src/app/api/generators/cpf/route.ts
@@ -1,33 +1,7 @@
+import { generateCPF } from '@/lib/generate-cpf'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  function calculateDigit(partialCPF: string) {
-    let sum = 0
-    let weight = partialCPF.length + 1
-
-    for (const num of partialCPF) {
-      sum += Number.parseInt(num) * weight--
-    }
-
-    const remained = sum % 11
-    return remained < 2 ? '0' : (11 - remained).toString()
-  }
-
-  function generateCPF(formatted = true) {
-    const partialCPF = Array.from({ length: 9 }, () =>
-      Math.floor(Math.random() * 10)
-    ).join('')
-
-    const firstDigit = calculateDigit(partialCPF)
-    const secondDigit = calculateDigit(partialCPF + firstDigit)
-
-    const cpfCompleto = partialCPF + firstDigit + secondDigit
-
-    return formatted
-      ? `${cpfCompleto.slice(0, 3)}.${cpfCompleto.slice(3, 6)}.${cpfCompleto.slice(6, 9)}-${cpfCompleto.slice(9)}`
-      : cpfCompleto
-  }
-
   const generatedCPF = generateCPF()
 
   return NextResponse.json({ cpf: generatedCPF })
