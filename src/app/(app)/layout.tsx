0105b072f6571b84d57272f99da8fa3fb9f4108e
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-12-10 15:17:45 +0100

    Add layout and providers for app structure; update dependencies

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,32 @@
+import type React from 'react'
+import { AppSidebar } from '@/components/sidebar/app-sidebar'
+import { AppBreadcrumb } from '@/components/sidebar/breadcrumb'
+import { Separator } from '@/components/ui/separator'
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar'
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+
+          <AppBreadcrumb />
+        </header>
+
+        <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1 className="text-2xl font-bold font-mono">Featured tools</h1>
+    </div>
+  )
+}
