459644755dda876528e8b356e37468e2c30e77a5
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-12-10 21:04:44 +0100

    Add React Query integration with QueryClientProvider and create getQueryClient utility

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,14 @@
 'use client'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'next-themes'
+import { getQueryClient } from '@/lib/react-query'
 
 export function Providers({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const queryClient = getQueryClient()
+
   return (
     <ThemeProvider
       attribute="class"
@@ -12,7 +16,7 @@ export function Providers({
       enableSystem
       disableTransitionOnChange
     >
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </ThemeProvider>
   )
 }
