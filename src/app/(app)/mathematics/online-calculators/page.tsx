4291e6e9a46461476cc44f0de9a6e02acc27d3a2
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-12-11 19:14:36 +0100

    Refactor CalculatorsPage to use online-calculators category and update heading

--- src/app/(app)/mathematics/online-calculators/page.tsx
+++ src/app/(app)/mathematics/online-calculators/page.tsx
@@ -2,16 +2,18 @@ import { tools } from '@/app/tools'
 import { ToolCard } from '@/components/tool-card'
 
 export default function CalculatorsPage() {
-  const calculators = tools.filter(
-    (tools) => tools.category === 'calculators'
-  )[0].items
+  const calculatorsTools =
+    tools
+      .flatMap(({ menuItems }) => menuItems)
+      .find(({ category }) => category === 'online-calculators')
+      ?.subMenuItems || []
 
   return (
     <div className="space-y-4">
-      <h1 className="text-2xl font-bold font-mono">Calculators</h1>
+      <h1 className="text-2xl font-bold font-mono">Online Calculators</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
-        {calculators.map((tool) => (
+        {calculatorsTools.map((tool) => (
           <ToolCard key={tool.title} {...tool} />
         ))}
       </div>
