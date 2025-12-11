4dacd6b94dfbf744d7848a7b6aaed0061ebee32a
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-12-11 12:22:58 +0100

    Add Home component to display featured tools with layout

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,20 @@
+import { ToolCard } from '@/components/tool-card'
+import { tools } from '../tools'
+
 export default function Home() {
+  const allTools = tools
+    .flatMap(({ menuItems }) => menuItems)
+    .flatMap(({ subMenuItems }) => subMenuItems)
+
   return (
     <div className="flex flex-col gap-y-4">
       <h1 className="text-2xl font-bold font-mono">Featured tools</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+        {allTools.map((tool) => (
+          <ToolCard key={tool.url} {...tool} />
+        ))}
+      </div>
     </div>
   )
 }
